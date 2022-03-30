573f7ad3d402b1d37e48416dda6596f118553214
AdilzaMattos <adilzamattos@gmail.com>
2022-03-30 14:46:50 -0300

    First commit

---
+++ src/main/java/br/com/fiap/abdtechservice/AbdTechServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.abdtechservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdTechServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbdTechServiceApplication.class, args);
+	}
+
+}
